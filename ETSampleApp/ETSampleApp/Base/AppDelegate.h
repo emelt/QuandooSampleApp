e08468af0fef5848349d944523b63eef566a757d
Emel Topaloglu <Emel.Topaloglu@monitise.com>
2015-05-09 20:00:48 +0300

    Created initial project structure with 3rd party support

---
+++ ETSampleApp/ETSampleApp/Base/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ETSampleApp
+//
+//  Created by Emel Topaloglu on 09/05/2015.
+//  Copyright (c) 2015 Emel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
